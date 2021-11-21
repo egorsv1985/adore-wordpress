2497ddab975dbe39ba6bef385a23a4f8fa45887b
egorsv1985 <egorvs2009@gmail.com>
2021-11-21 22:07:45 +0300

    fix

--- header.php
+++ header.php
@@ -6,15 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	 <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/fonts/fonts.css">    
-    <title>
-	 <p><?php bloginfo('description'); ?> </p></title>
+    <title><?php echo bloginfo('description'); ?></title>
 	 <?php wp_head(); ?>
 </head>
 
 <body>    
     <header class="header">
         <div class="header__wrapper container">          
-                <a class="header__logo logo" href="#">
+                <a class="header__logo logo" href="<?php bloginfo('url'); ?>">
                     <img class="logo__img" src="<?php echo IMG_DIR; ?> ./header/header-logo.svg" alt="logo">
                 </a>
                 <ul class="header__contacts">
