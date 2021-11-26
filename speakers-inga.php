fdc0d5084cc627493b870044a4cefabc990f0e70
egorsv1985 <egorvs2009@gmail.com>
2021-11-27 00:04:56 +0300

    added inga

---
+++ speakers-inga.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Template Name: programma
+*/
+?>
+<?php get_header(); ?>
+<section class="speakerCards">
+       
+        <div class="container">
+      <button class="speakerCards__btn">
+		<img class="program_arrow" src="<?php echo IMG_DIR; ?>/form/Arrow.svg" alt="arrow">
+			</button>
+      </div>
+      <div class="speakerCards__wrap">
+        <div class="container"> 
+            <div class="speakerCards__wrapper">
+       <img class="speakerCards__img" src="<?php echo IMG_DIR; ?>/speakers/speaker1.png" alt="">
+       <div class="speakerCards__name">
+           <h3 class="speakerCards__subtitle">Кристина Трофимова</h3>
+			  <a href="https://instagram.com/trofimova.prokis?utm_medium=copy_link" class="speakerCards__link">
+                            <img class="speakerCards__icon" src="<?php echo IMG_DIR; ?>/header/Instagram.svg" alt="Instagram">
+                        </a>
+       </div>
+<p class="speakerCards__text">•Женский спикер. Автор женских трансформационных программ по раскрытию сексуальности и женственности: Осознанный секс и PROбуждение.<br>
+    •800+ академических часов обшей практики. 250+ часов практики работы с клиентами. <br>
+    Образование:
+    •БГПУ им. М.Танка: педагог социальный <br>
+    •Киевский национальный университет им Драгоманова: основы сексологии и сексопатологии <br>
+    •ЕDPRO Международная академия дополнительного образования: психологическое консультирование в сексологии (сейчас доучиваюсь)<br>
+    •Курсы Фаза Роста 
+    </p>
+</div>
+</div>
+</div>
+
+    </section> 
+<?php get_footer(); ?>
