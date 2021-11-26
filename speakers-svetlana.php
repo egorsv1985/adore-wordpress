a3bb442d60a852fcf50fb30e9e31998e12a8fe6c
Sabancev Egor <60539979+egorsv1985@users.noreply.github.com>
2021-11-26 23:12:11 +0300

    added speakers

---
+++ speakers-svetlana.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: programma
+*/
+?>
+<?php get_header(); ?>
+<section class="speakerCards">
+       
+        <div class="container">
+      <button class="speakerCards__btn">
+		<img class="program_arrow" src="<?php echo IMG_DIR; ?>/form/Arrow.svg" alt="arrow">
+			</button>
+      </div>
+      <div class="speakerCards__wrap">
+        <div class="container"> 
+            <div class="speakerCards__wrapper">
+       <img class="speakerCards__img" src="<?php echo IMG_DIR; ?>/speakers/speaker2.png" alt="">
+       <div class="speakerCards__name">
+           <h3 class="speakerCards__subtitle">Светлана Размыслович</h3>
+			  <a href="https://instagram.com/svetlana_razmyslovich?utm_medium=copy_link" class="speakerCards__link">
+                            <img class="speakerCards__icon" src="<?php echo IMG_DIR; ?>/speaker-cards/icon-instagram.png" alt="Instagram">
+                        </a>
+       </div>
+<p class="speakerCards__text">•Выпускница и лидер ЦПТ (Центра психологических технологий) г.Киева<br>
+•Автор курсов по личностному росту и саморазвитию женщин<br>
+•Консультант и участник многих общественных и бизнес-объединений<br>
+•В прошлом успешный сетевик (ранг Золотого директора) и соавтор "Пособия по продажам"<br>
+•Участник 14 потока проекта "Метамофрозы"<br>
+•Руководитель своего event-агенства "SeeYou"<br>
+•Инвестор на пути к своему 1млн $<br>
+•Владелец сети апартаментов г. Минска "Helen Apartments"<br>
+
+Смогла я, сможешь и ты!
+    </p>
+</div>
+</div>
+</div>
+
+    </section> 
+<?php get_footer(); ?>
---
+++ speakers-tatyana.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: programma
+*/
+?>
+<?php get_header(); ?>
+<section class="speakerCards">
+       
+        <div class="container">
+      <button class="speakerCards__btn">
+		<img class="program_arrow" src="<?php echo IMG_DIR; ?>/form/Arrow.svg" alt="arrow">
+			</button>
+      </div>
+      <div class="speakerCards__wrap">
+        <div class="container"> 
+            <div class="speakerCards__wrapper">
+       <img class="speakerCards__img" src="<?php echo IMG_DIR; ?>/speakers/speaker4.png" alt="">
+       <div class="speakerCards__name">
+           <h3 class="speakerCards__subtitle">Татьяна Чернявская</h3>
+			  <a href="https://instagram.com/cherniavskaya_tatiana_naila?utm_medium=copy_link" class="speakerCards__link">
+                            <img class="speakerCards__icon" src="<?php echo IMG_DIR; ?>/speaker-cards/icon-instagram.png" alt="Instagram">
+                        </a>
+       </div>
+<p class="speakerCards__text">•Открыла первый бизнес c 50$<br>
+•Обладательница 2 Кубков Мира<br>
+•Вырастила 4 поколения чемпионов мира<br>
+•Автор курса по ресурсному состоянию и энергии<br>
+•Организовала более 150 мероприятий<br>
+•Победитель бизнес-обучения “Скорость” в Беларуси<br>
+•Лучший молодежный проект Демо дня в Москве<br>
+•Дважды номинант премии Человек года<br>
+•Учусь в наставничестве у долларового миллионера<br>
+    </p>
+</div>
+</div>
+</div>
+
+    </section> 
+<?php get_footer(); ?>
