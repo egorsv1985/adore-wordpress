bfe361c8867f25df3f26505d08e6e5c2859cecbd
Sabancev Egor <60539979+egorsv1985@users.noreply.github.com>
2021-12-03 15:24:51 +0300

    fix bags in programma test

---
+++ programma-test.php
@@ -0,0 +1,167 @@
+<?php
+/*
+Template Name: programma
+*/
+?>
+<?php get_header(); ?>
+<section class="program">
+	<h2 class="program__title">ПРОГРАММА</h2>
+		<div class="container">
+		<a href="https://adore.by/?page_id=10" class="program_btn">
+			<img class="program_arrow" src="<?php echo IMG_DIR; ?>/form/Arrow.svg" alt="">
+		</a>
+		</div>
+	<div class="program__wrapper"> <!-- поменял класс		 -->
+		<div class="program__wrap"> <!-- поменял класс		 -->
+			<img class="program__img" src="<?php echo IMG_DIR; ?>/program/program.jpg"  alt="">
+		<div class="program__description"> <!-- поменял класс		 -->
+		 	<p class="program__time">17:00 - 18:00</p>
+		 	<p class="program__text">Приветственный фуршет. Первый женский нетворкинг</p>
+		</div>
+		<div class="program__description"> <!-- поменял класс		 -->
+			<p class="program__time">18:10 - 18:40</p>
+			<p class="program__text">Кристина Трофимова: "Как разнообразить сексуальную жизнь. Секс-гаджеты: как выбрать для себя. Какие есть"</p>
+		</div>
+		<div class="program__description"> <!-- поменял класс		 -->
+			<p class="program__time">18:50 - 19:20</p>
+			<p class="program__text">Светлана Размыслович: "Как понять, чего я хочу  и воплотить это в реальность?"</p>
+		</div>
+		<div class="program__description"> <!-- поменял класс		 -->
+			<p class="program__time">19:30 - 20:00</p>
+			<p class="program__text">Второй женский нетворкинг. Музыкальная пауза</p>
+		</div>
+		<div class="program__description"> <!-- поменял класс		 -->
+			<p class="program__time">20:00 - 20:30</p>
+			<p class="program__text">Инга Медведева: "Внешний вид и внутренний мир. Преображение, после которого выходят на новый уровень"</p>
+		</div>
+		<div class="program__description"> <!-- поменял класс		 -->
+			<p class="program__time">20:30 - 20:40</p>
+			<p class="program__text">Официальное открытие женского клуба "Adore"</p>
+		</div>
+		<div class="program__description"> <!-- поменял класс		 -->
+			<p class="program__time">20:40 - 21:00</p>
+			<p class="program__text">Татьяна Чернявская: "Как активировать внутреннюю энергию и начать управлять своей жизнью"				</p>
+		</div>
+		<div class="program__description"> <!-- поменял класс		 -->
+			<p class="program__time">21:00 - 23:00</p>
+			<p class="program__text">Предновогодний фуршет. Розыгрыш подарков и сертификатов. Танцевальный бал и шоу-вечеринка.</p>
+		</div>
+		<h3 class="program__subtitle">Общение в приятной компании, заряд эмоций и вдохновение, ощущение легкости, красоты, изящества и атмосфера праздника</h3>
+		<img class="program__image" src="<?php echo IMG_DIR; ?>/program/hall.jpg" alt="">
+		<h4 class="program__address">Мероприятие будет проходить по адресу: ул. Газеты Правды 11</h4>
+	</div>
+	</div>
+	</div>
+<section class="person">
+	<div class="container">
+		<h3 class="person__title">ВЕДУЩИЙ ПРОГРАММЫ</h3>
+		<div class="person__pseudo">
+			<img class="person__img" src="<?php echo IMG_DIR; ?>/person/Artem.png" alt="Artem">
+		</div>
+		
+		<div class="person__wrap">
+			<p class="person__text">Артем Астровлян </p>
+			<a href="https://instagram.com/astrovlian?utm_medium=copy_link" class="speakerCards__link">
+                            <img class="person__icon" src="<?php echo IMG_DIR; ?>/person/icon.png" alt="instagram">
+                        </a>
+			
+		</div>	
+		</div>	
+		<div class="person__dressCode">			 
+			 <div class="person__dressCode__wrapper">
+				  <div class="person__dressCode__border">
+				  <h3 class="person__dressCode__title">ДРЕСС-КОД</h3>
+				  <div class="person__dressCode__wrap">
+						<img class="person__dressCode__img" src="<?php echo IMG_DIR; ?>/dressCode/dressCode.jpg" alt="">
+						<div class="person__dressCode__wrap--right">
+							 <div class="person__dressCode__content">
+								  <p class="person__dressCode__text--top">Вечерние платья у девушек</p>
+								  <img class="person__dressCode__img--top" src="<?php echo IMG_DIR; ?>/dressCode/dressCodeTop.png" alt=""> 
+							 </div>
+							 <div class="person__dressCode__content">
+								  <img class="person__dressCode__img--bottom" src="<?php echo IMG_DIR; ?>/dressCode/dressCodeBottom.png" alt="">
+								  <p class="person__dressCode__text--bottom">Нарядные костюмы
+										у мужчин</p>
+							 </div>
+						</div>
+				  </div>
+			 </div>
+		</div>
+		</div>
+		
+	
+		<div class="person__present">
+			 <div class="container">
+			 <div class="person__present__wrapper">
+				  <div class="person__present__border">
+				  <h3 class="person__present__title">ПОДАРКИ</h3>
+				  <div class="person__present__wrap">
+						<img class="person__present__img" src="<?php echo IMG_DIR; ?>/present/present.jpg" alt="">
+						<div class="person__present__wrap--right">
+							 <div class="person__present__content">
+								  <p class="person__present__text--right">Каждой девушке личный box с подарками от наших партнеров</p>
+								 </div>
+							</div>
+				 		 </div>
+					 </div>
+				</div>
+		</div>
+	<div class="person__speakers speakers">
+		<div class="container">
+			<h3 class="person__title">СПИКЕРЫ</h3>
+			<div class="person__pseudo">
+				<img class="person__img" src="<?php echo IMG_DIR; ?>/person/kristina.png" alt="Кристина">
+			</div>		
+			<div class="person__wrap--speker">
+				<p class="person__text">Кристина Трофимова </p>
+				<a href="https://instagram.com/trofimova.prokis?utm_medium=copy_link" class="speakerCards__link">
+               		<img class="person__icon" src="<?php echo IMG_DIR; ?>/person/icon.png" alt="instagram">
+            	</a>			
+			</div>	
+			<h4 class="speakers__word">СЕКСОЛОГ</h4>
+			<h4 class="speakers__word">КАК РАЗНООБРАЗИТЬ СЕКСУАЛЬНУЮ ЖИЗНЬ. СЕКС-ГАДЖЕТЫ: КАК ВЫБРАТЬ ДЛЯ СЕБЯ. КАКИЕ ЕСТЬ.				</h4>			
+			<a href="https://adore.by/?page_id=69" class="speakers__btn">Подробнее</a>
+		
+		<div class="person__pseudo">
+			<img class="person__img" src="<?php echo IMG_DIR; ?>/person/svetlana.png" alt="svetlana">
+		</div>		
+		<div class="person__wrap--speker">
+			<p class="person__text">Светлана Размыслович</p>
+			<a href="https://instagram.com/svetlana_razmyslovich?utm_medium=copy_link" class="speakerCards__link">
+               <img class="person__icon" src="<?php echo IMG_DIR; ?>/person/icon.png" alt="instagram">
+            </a>			
+		</div>	
+		<h4 class="speakers__word">ЖЕНСКИЙ КОУЧ</h4>
+			 <h4 class="speakers__word">КАК ПОНЯТЬ, ЧЕГО Я ХОЧУ И ВОПЛОТИТЬ ЭТО В РЕАЛЬНОСТЬ?</h4>
+			
+		<a href="https://adore.by/?page_id=74" class="speakers__btn">Подробнее</a>
+			<div class="person__pseudo">
+			<img class="person__img" src="<?php echo IMG_DIR; ?>/person/inga.png" alt="inga">
+		</div>		
+		<div class="person__wrap--speker">
+			<p class="person__text">Инга Медведева</p>
+			<a href="https://instagram.com/medvedeva_inga?utm_medium=copy_link" class="speakerCards__link">
+               <img class="person__icon" src="<?php echo IMG_DIR; ?>/person/icon.png" alt="instagram">
+            </a>			
+		</div>	
+		<h4 class="speakers__word">СТИЛИСТ-ПСИХОЛОГ</h4>
+			 <h4 class="speakers__word">ВНЕШНИЙ ВИД И ВНУТРЕННИЙ МИР. ПРЕОБРАЖЕНИЕ, ПОСЛЕ КОТОРОГО  ВЫХОДЯТ НА НОВЫЙ УРОВЕНЬ.</h4>
+			
+		<a href="https://adore.by/?page_id=80" class="speakers__btn">Подробнее</a>
+			<div class="person__pseudo">
+			<img class="person__img" src="<?php echo IMG_DIR; ?>/person/tatyana.png" alt="tatiana">
+		</div>		
+		<div class="person__wrap--speker">
+			<p class="person__text">Татьяна Чернявская</p>
+			<a href="https://instagram.com/cherniavskaya_tatiana_naila?utm_medium=copy_link" class="speakerCards__link">
+               <img class="person__icon" src="<?php echo IMG_DIR; ?>/person/icon.png" alt="instagram">
+            </a>			
+		</div>	
+		<h4 class="speakers__word">ЖЕНСКИЙ ТРЕНЕР</h4>
+			 <h4 class="speakers__word">КАК АКТИВИРОВАТЬ ВНУТРЕННЮЮ ЭНЕРГИЮ И НАЧАТЬ УПРАВЛЯТЬ СВОЕЙ ЖИЗНЬЮ</h4>
+			
+		<a href="https://adore.by/?page_id=77" class="speakers__btn">Подробнее</a>
+		</div>	
+	</section>
+	</section>
+<?php get_footer(); ?>
